881c504729c6de159b01fe8919ecd89ec4ca3501
ariexxe <arielleperez146@gmail.com>
2024-06-25 15:16:48 +0800

    add home page

--- BAIPetRegMobileApp/Views/HomePage.xaml.cs
+++ BAIPetRegMobileApp/Views/HomePage.xaml.cs
@@ -5,5 +5,11 @@ public partial class HomePage : ContentPage
 	public HomePage()
 	{
 		InitializeComponent();
+
 	}
+
+    private void BtnRegisterPet_Clicked(object sender, EventArgs e)
+    {
+
+    }
 }
