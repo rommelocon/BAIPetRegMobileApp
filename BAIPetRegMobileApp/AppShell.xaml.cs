eca159f5edd4a544f51c91eba578ae7b26114b85
ariexxe <arielleperez146@gmail.com>
2024-06-27 08:46:15 +0800

    add petRegister Page

--- BAIPetRegMobileApp/AppShell.xaml.cs
+++ BAIPetRegMobileApp/AppShell.xaml.cs
@@ -11,6 +11,7 @@ namespace BAIPetRegMobileApp
             Routing.RegisterRoute(nameof(LoginPage), typeof(LoginPage));
             Routing.RegisterRoute(nameof(HomePage), typeof(HomePage));
             Routing.RegisterRoute(nameof(RegisterPage), typeof(RegisterPage));
+            Routing.RegisterRoute(nameof(PetRegisterPage), typeof(PetRegisterPage));
         }
     }
 }
--- BAIPetRegMobileApp/Views/HomePage.xaml.cs
+++ BAIPetRegMobileApp/Views/HomePage.xaml.cs
@@ -11,7 +11,7 @@ public partial class HomePage : ContentPage
 
     private void BtnRegisterPet_Clicked(object sender, EventArgs e)
     {
-		Shell.Current.GoToAsync(nameof(RegisterPage));
+		Shell.Current.GoToAsync(nameof(PetRegisterPage));
     }
 
     private void BtnLogout_Clicked(object sender, EventArgs e)
