23850e8924c63b2cc33febebb8701221cb42aba5
GitHubNexon <johnmarkpulmano052802@gmail.com>
2024-07-11 16:53:25 +0800

    updated the breed list of dog

--- BAIPetRegMobileApp/Views/PetRegisterPage.xaml.cs
+++ BAIPetRegMobileApp/Views/PetRegisterPage.xaml.cs
@@ -1,37 +1,168 @@
 using SQLite;
+using System.Windows.Input;
 
 namespace BAIPetRegMobileApp.Views;
 
 public partial class PetRegisterPage : ContentPage
 {
     public string selectedOwnership;
+
+    List<string> dogBreeds = new List<string>()
+{
+    "Affenpinscher",
+    "Afghan Hound",
+    "Golden Retriever",
+    "Airedale Terrier",
+    "Akita Inu",
+    "Alaskan Malamute",
+    "ASPIN",
+    "Australian Cattle Dog",
+    "Australian Dingo",
+    "Australian Kelpie",
+    "Australian Shepherd",
+    "Australian Silky Terrier",
+    "Australian Terrier",
+    "Basenji",
+    "Basset Hound",
+    "Beagle",
+    "Bearded Collie",
+    "Bedlington",
+    "Belgian Malinois",
+    "Bernese Mountain Dog",
+    "Bichon Frise",
+    "Bloodhound",
+    "Border",
+    "Border Collie",
+    "Borzoi",
+    "Boston Terrier",
+    "Boxer",
+    "Briard",
+    "British Bulldog",
+    "Bull",
+    "Bull Mastiff",
+    "Cairn",
+    "Cavalier King Charles",
+    "Chihuahua",
+    "Chinese Crested Dog",
+    "Chow Chow",
+    "Cocker Spaniel",
+    "Curly Coated Retriever",
+    "Dachshund",
+    "Dalmatian",
+    "Deerhound",
+    "Doberman Pinscher",
+    "Dogue de Bordeaux",
+    "English Setter",
+    "English Springer Spaniel",
+    "Field Spaniel",
+    "Finnish Spitz",
+    "Foxhound",
+    "Fox Terrier",
+    "French Bulldog",
+    "German Pinscher",
+    "German Pointer",
+    "German Shepherd",
+    "Golden Retriever",
+    "Gordon Setter",
+    "Great Dane",
+    "Greyhound",
+    "Hungarian Vizsla",
+    "Irish Setter",
+    "Irish Red and White Setter",
+    "Irish Water Spaniel",
+    "Irish Wolfhound",
+    "Italian Greyhound",
+    "Jack Russell",
+    "Japanese Chin",
+    "Keeshond",
+    "Kerry Blue",
+    "Labrador Retriever",
+    "Lhasa Apso",
+    "Lowchen",
+    "Maltese",
+    "Miniature Pinscher",
+    "Miniature Poodle",
+    "Mixed",
+    "Newfoundland",
+    "Norwich Terrier",
+    "Old English Sheepdog",
+    "Papillon",
+    "Pekingese",
+    "Pharaoh Hound",
+    "Pointer",
+    "Pomeranian",
+    "Portuguese Water Dog",
+    "Pug",
+    "Puli",
+    "Pyrenean Mountain Dog",
+    "Rhodesian Ridgeback",
+    "Rottweiler",
+    "Rough Collie",
+    "Saluki",
+    "Samoyed",
+    "Schnauzer",
+    "Schipperke",
+    "Scottish Terrier",
+    "Shar Pei",
+    "Shetland Sheepdog",
+    "Shih Tzu",
+    "Siberian Husky",
+    "Smooth Collie",
+    "St. Bernard",
+    "Staffordshire Bull Terrier",
+    "Standard Poodle",
+    "Tibetan Terrier",
+    "Tibetan Spaniel",
+    "Toy Poodle",
+    "Weimaraner",
+    "Welsh Corgi",
+    "West Highland White Terrier",
+    "Whippet",
+    "Yorkshire Terrier"
+};
+    List<string> catBreeds = new List<string>()
+{
+    "Siamese",
+    "Maine Coon",
+    "Persian",
+    "Bengal",
+    "Sphynx"
+};
+
+    List<string> selectedBreeds = new List<string>();
+
+
     public PetRegisterPage()
     {
         InitializeComponent();
 
+
+
+        List<string> listSpecies = new List<string>()
+            {
+                "Dog",
+                "Cat"
+            };
+
+        speciesList.ItemsSource = listSpecies;
+        breedList.ItemsSource = new List<string> { "Please pick a species first" };
+
+
         List<string> listOwnership = new List<string>()
         {
-            "Ownership001",
-            "Ownership002",
-            "Ownership003"
+            "Personal Ownership",
+            "Family Ownership",
+            "Shared Ownership",
+            "Foster Ownership",
+            "Service or Assistance",
+            "Working Animals",
+            "Breeder Ownership",
+            "Rescue Organization"
+
         };
         ownershipList.ItemsSource = listOwnership;
 
-        List<string> listSpecies = new List<string>()
-        {
-            "Species001",
-            "Species002",
-            "Species003"
-        };
-        speciesList.ItemsSource = listSpecies;
 
-        List<string> listBreed = new List<string>()
-        {
-            "Breed001",
-            "Breed002",
-            "Breed003"
-        };
-        breedList.ItemsSource = listBreed;
 
         List<string> listSex = new List<string>()
         {
@@ -39,7 +170,7 @@ public partial class PetRegisterPage : ContentPage
             "Female"
         };
         sexList.ItemsSource = listSex;
-        
+
     }
 
     public void OnOwnershipListSelectedIndexChanged(object sender, EventArgs e)
@@ -96,6 +227,30 @@ public partial class PetRegisterPage : ContentPage
         Shell.Current.GoToAsync(nameof(FinalCheckingPage));
     }
 
-   
+    private void OnSpeciesSelected(object sender, EventArgs e)
+    {
+        var picker = (Picker)sender;
+        var selectedSpecies = picker.SelectedItem as string;
+
+
+        if (selectedSpecies == "Dog")
+        {
+
+            breedList.ItemsSource = dogBreeds;
+        }
+        else if (selectedSpecies == "Cat")
+        {
+
+            breedList.ItemsSource = catBreeds;
+        }
+        else
+        {
+            breedList.ItemsSource = new List<string> { "Please pick a species first" };
+        }
+    }
+
+
+
+
 
 }
